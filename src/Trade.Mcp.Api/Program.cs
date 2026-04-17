e9ab3866532fd51c46e7c67f424a288ebad69e27
Eugene Murray <eugene.murray@gmail.com>
2026-04-17 22:37:08 +0100

    update for openapi

--- src/Trade.Mcp.Api/Program.cs
+++ src/Trade.Mcp.Api/Program.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Scalar.AspNetCore;
 using Serilog;
 using Trade.Application.Common.Services;
 using Trade.Infrastructure;
@@ -25,6 +26,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 builder.Services.AddProblemDetails();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddOpenApi();
 builder.Services.AddScoped<ITradePerformanceCalculator, TradePerformanceCalculator>();
 builder.Services.AddTradeInfrastructure(builder.Configuration);
 builder.Services.AddHealthChecks()
@@ -41,6 +43,12 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 
 app.UseExceptionHandler();
 app.UseSerilogRequestLogging();
